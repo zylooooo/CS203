83d8ba7c9de08edc5fd8ca5c337d546cad75926b
John Doe <djcheung.2023@scis.smu.edu.sg>
2024-09-09 23:23:37 +0800

    Added redirection to /user/hello upon successful login
    Upon logging in the backend, no longer will experience the whitelabel error page. Instead, redirection to user/hello will happen

---
+++ backend/src/main/java/com/example/backend/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,25 @@
+package com.example.backend.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        String redirectURL = request.getContextPath();
+
+        if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+            redirectURL = "/admin/home";
+        } else if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_USER"))) {
+            redirectURL = "/user/hello";
+        }
+
+        response.sendRedirect(redirectURL);
+    }
+}
+
