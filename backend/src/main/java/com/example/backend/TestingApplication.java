b08b58760c2b0ee849de5f1247b70251de38c242
zhi yi <zhiyi.loo.2023@scis.smu.edu.sg>
2024-09-09 20:03:55 +0800

    try fixing the login page not routing to anywhere

--- backend/src/main/java/com/example/backend/TestingApplication.java
+++ backend/src/main/java/com/example/backend/TestingApplication.java
@@ -2,8 +2,10 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.backend")
 public class TestingApplication {
 
 	public static void main(String[] args) {
