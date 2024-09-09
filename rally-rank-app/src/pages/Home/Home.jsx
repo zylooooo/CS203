41dddadce817f7187e47bc5e807db2308b34ae7d
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-09-09 23:39:43 +0800

    Added NavBar.jsx and Home.jsx for testing

---
+++ rally-rank-app/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className="home-container">
+            <h1 className="home-title">Welcome to Home Page</h1>
+        </div>
+    );
+}
+
+export default Home;
