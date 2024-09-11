34424810ecedc9e9175e72c2d6cd82d60e1436a1
John Doe <djcheung.2023@scis.smu.edu.sg>
2024-09-11 16:34:40 +0800

    Fixed redirection logic for localhost:8080/login and localhost:8080/
    Now for users who have already logged in, check for authentication. If the user has already logged in, entering the localhost:8080 or /login into the browser will no longer bring the user back to login but straight to hello

--- backend/src/main/java/com/example/backend/controller/LoginController.java
+++ backend/src/main/java/com/example/backend/controller/LoginController.java
@@ -2,11 +2,12 @@ package com.example.backend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login() {
-        return "login";
+    public String login(Authentication authentication) {
+        return authentication != null ? "redirect:/user/hello" : "login";
     }
 }
