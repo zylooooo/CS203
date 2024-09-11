c8f0c78cfa33c2871d622888bb3a179f3778cdde
John Doe <djcheung.2023@scis.smu.edu.sg>
2024-09-11 18:39:05 +0800

    Updated logic of redirecting
    Updated the logic of redirecting if user is authenticated, redirect back to the page he came from

--- backend/src/main/java/com/example/backend/controller/LoginController.java
+++ backend/src/main/java/com/example/backend/controller/LoginController.java
@@ -1,13 +1,19 @@
 package com.example.backend.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.security.core.Authentication;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login(Authentication authentication) {
-        return authentication != null ? "redirect:/user/hello" : "login";
+    public String login(Authentication authentication, HttpServletRequest request) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            String referer = request.getHeader("Referer");
+            return referer != null ? "redirect:" + referer : "redirect:/user/hello";
+        }
+        return "login";
     }
 }
