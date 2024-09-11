817c3a68d4a035309edc977730b2b1bbe4c1ed61
zhi yi <zhiyi.loo.2023@scis.smu.edu.sg>
2024-09-11 21:27:03 +0800

    trying to redirect back to /hello after acccessing /login from there (not fixed)

--- backend/src/main/java/com/example/backend/controller/HelloController.java
+++ backend/src/main/java/com/example/backend/controller/HelloController.java
@@ -1,13 +1,13 @@
 package com.example.backend.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, Spring Boot!";
+        return "hello";
     }
 }
