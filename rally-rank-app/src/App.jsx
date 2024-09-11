12b1ce55a9a167b0c2027f2c8cfa4aabd582a0b2
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-09-11 15:38:14 +0800

    Edited Login.jsx

--- rally-rank-app/src/App.jsx
+++ rally-rank-app/src/App.jsx
@@ -1,9 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Home/Login";
+import PlayerLogin from "./pages/Home/Login";
 import "./index.css";
 
 function App() {
-    return <Login />;
+    return (
+        <div className="h-screen flex flex-col justify-center items-center">
+            <PlayerLogin />
+        </div>
+    );
 }
 
 export default App;
