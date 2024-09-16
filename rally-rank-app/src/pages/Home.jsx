570f23e5f4afab42244b86aee959331f6a279f26
f4heemmk.2023 <f4heemmmm@gmail.com>
2024-09-16 18:16:23 +0800

    Edited Home.jsx

--- rally-rank-app/src/pages/Home.jsx
+++ rally-rank-app/src/pages/Home.jsx
@@ -1,32 +1,41 @@
 function Home() {
   return (
-    <div className="home-container flex w-full p-9 gap-2 justify-evenly">
-      <div className="row-container flex flex-col w-3/5 gap-8">
-        <div>
-          <h2 className="A">Your Scheduled Tournaments</h2>
-          <button className="join-button px-4 py-2 border border-gray-300 text-sm whitespace-nowrap w-full min-height-300px">
-            join tournamnet
-          </button>
+    <div className = "home-container flex w-full p-9 gap-2 justify-evenly">
+
+      {/* ROW CONTAINER: SCHEDULED TOURNAMENTS AND JOIN TOURNAMENTS */}
+      <div className = "row-container flex flex-col w-3/5 gap-8">
+
+        {/* SCHEDULED TOURNAMENTS CONTAINER */}
+        <div className = "scheduled-tournaments-container">
+          <h2 className = "mb-2"> Your Scheduled Tournaments </h2>
+          <div className = "p-4 bg-blue-500 text-white rounded shadow-lg border">
+            You have no scheduled tournaments. Join a tournament today!
+          </div>
         </div>
 
-        <div>
-          <h2 className="A"> Join a tournament today! </h2>
-          <button className="join-button px-4 py-2 border border-gray-300 text-sm whitespace-nowrap w-full min-height-300px">
-            join tournamnet
-          </button>
+        {/* JOIN TOURNAMENTS CONTAINER */}
+        <div className = "join-tournament-container">
+          <h2 className = "mb-2"> Join a tournament today! </h2>
+            <button className = "join-button px-4 py-2 border border-gray-300 text-sm whitespace-nowrap w-full min-height-300px">
+              Join tournament!
+            </button>
         </div>
-      </div>
-      <div className="column-container gap-5">
-        <h2 className="A">Tournament Leaderboard</h2>
-        <div className="switch-ranking flex gap-8">
-          <button>Top</button>
-          <button>You</button>
-        </div>
-        <button className="join-button px-4 py-2 border border-gray-300 text-sm whitespace-nowrap w-full min-height-600px min-w-72">
-          leaderboard
-        </button>
-      </div>
     </div>
+
+      {/* COLUMN CONTAINER: LEADERBOARD CONTAINER */}
+      <div className = "col-container gap-8">
+        <div className = "leaderboard container">
+          <h2> Tournament Leaderboard </h2>
+          <div className = "leaderboard-switch-view-container flex gap-10 mb-2">
+            <button> Top </button>
+            <button> You </button>
+          </div>
+          <div className = "leaderboard-box bg-gray-100 border border-gray-300 text-sm text-center w-full h-72 min-w-72 flex items-center justify-center min-height-500px">
+            Add leaderboard rankings here
+          </div>
+        </div>
+      </div>
+  </div>
   );
 }
 
