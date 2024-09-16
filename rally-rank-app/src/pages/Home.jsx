e7bd7050a6ecaed7668c638d1f42604e8e6700bf
Tori <tori.ng.2023@scis.smu.edu.sg>
2024-09-16 17:14:22 +0800

    Edited leaderboard

--- rally-rank-app/src/pages/Home.jsx
+++ rally-rank-app/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
-    <div className="home-container flex w-full p-9 gap-8 justify-around">
-      <div className="row-container flex flex-col w-9/12 gap-8">
+    <div className="home-container flex w-full p-9 gap-2 justify-evenly">
+      <div className="row-container flex flex-col w-3/5 gap-8">
         <div>
           <h2 className="A">Your Scheduled Tournaments</h2>
           <button className="join-button px-4 py-2 border border-gray-300 text-sm whitespace-nowrap w-full min-height-300px">
@@ -16,8 +16,15 @@ function Home() {
           </button>
         </div>
       </div>
-      <div className="column-container">
+      <div className="column-container gap-5">
         <h2 className="A">Tournament Leaderboard</h2>
+        <div className="switch-ranking flex gap-8">
+          <button>Top</button>
+          <button>You</button>
+        </div>
+        <button className="join-button px-4 py-2 border border-gray-300 text-sm whitespace-nowrap w-full min-height-600px min-w-72">
+          leaderboard
+        </button>
       </div>
     </div>
   );
