dc17450898e83994e29ac562b36e0c0cb2745d71
Tori <tori.ng.2023@scis.smu.edu.sg>
2024-09-16 16:56:20 +0800

    Edited Home.jsx

--- rally-rank-app/src/pages/Home.jsx
+++ rally-rank-app/src/pages/Home.jsx
@@ -1,9 +1,26 @@
 function Home() {
-    return (
-        <div className="home-container">
-            <h1 className="home-title">Welcome to Home Page</h1>
+  return (
+    <div className="home-container flex w-full p-9 gap-8 justify-around">
+      <div className="row-container flex flex-col w-9/12 gap-8">
+        <div>
+          <h2 className="A">Your Scheduled Tournaments</h2>
+          <button className="join-button px-4 py-2 border border-gray-300 text-sm whitespace-nowrap w-full min-height-300px">
+            join tournamnet
+          </button>
         </div>
-    );
+
+        <div>
+          <h2 className="A"> Join a tournament today! </h2>
+          <button className="join-button px-4 py-2 border border-gray-300 text-sm whitespace-nowrap w-full min-height-300px">
+            join tournamnet
+          </button>
+        </div>
+      </div>
+      <div className="column-container">
+        <h2 className="A">Tournament Leaderboard</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
