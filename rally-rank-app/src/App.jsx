188add2cf35e96810820a6829748bf51b79df6ee
Tori <tori.ng.2023@scis.smu.edu.sg>
2024-09-17 18:13:50 +0800

    Reorganised files from 'pages' into PlayerView, AdminView and PublicView

--- rally-rank-app/src/App.jsx
+++ rally-rank-app/src/App.jsx
@@ -1,58 +1,57 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
-import MainLayout from "./pages/MainLayout";
-import Home from "./pages/Home";
-import { PlayerLogin, AdminLogin } from "./pages/Login";
-import { SignUp } from "./pages/SignUp";
-import { News } from "./pages/News";
+import MainLayout from "./components/MainLayout";
+import AdminLogin from "./AdminView/Login";
+import { News } from "./PublicView/News";
+import { Home, PlayerLogin, SignUp } from "./PlayerView";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <MainLayout>
-                            <Home />
-                        </MainLayout>
-                    }
-                />
-                <Route
-                    path="/login"
-                    element={
-                        <MainLayout>
-                            <PlayerLogin />
-                        </MainLayout>
-                    }
-                />
-                <Route
-                    path="/login-admin"
-                    element={
-                        <MainLayout>
-                            <AdminLogin />
-                        </MainLayout>
-                    }
-                />
-                <Route
-                    path="/signup"
-                    element={
-                        <MainLayout>
-                            <SignUp />
-                        </MainLayout>
-                    }
-                />
-                <Route
-                    path="/News"
-                    element={
-                        <MainLayout>
-                            <News />
-                        </MainLayout>
-                    }
-                />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <MainLayout>
+              <PlayerLogin />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/login-admin"
+          element={
+            <MainLayout>
+              <AdminLogin />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <MainLayout>
+              <SignUp />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/News"
+          element={
+            <MainLayout>
+              <News />
+            </MainLayout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ rally-rank-app/src/PlayerView/index.jsx
@@ -0,0 +1,5 @@
+import Home from "./Home";
+import PlayerLogin from "./Login";
+import { SignUp } from "./SignUp";
+
+export { Home, PlayerLogin, SignUp };
