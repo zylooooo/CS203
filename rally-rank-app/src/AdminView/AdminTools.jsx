bc1f3f20c19d0b012e46f354c55d032f0a54903c
Tori <tori.ng.2023@scis.smu.edu.sg>
2024-09-22 01:46:41 +0800

    Added conditional rendering for the NavBar

---
+++ rally-rank-app/src/AdminView/AdminTools.jsx
@@ -0,0 +1,9 @@
+function AdminTools() {
+    return (
+        <div>
+            <h1>Admin Tools</h1>
+        </div>
+    );
+}
+
+export default AdminTools;
--- rally-rank-app/src/components/MainLayout.jsx
+++ rally-rank-app/src/components/MainLayout.jsx
@@ -1,17 +1,28 @@
-//import NavBar from "./NavBar";
-import PublicNavBar from "./PublicNavBar"
+import NavBar from "./NavBar";
+import PublicNavBar from "./PublicNavBar";
+import AdminNavBar from "./AdminNavBar";
 
-function MainLayout({ children }) {
-    return (
-        <>
-            <div className="flex flex-col min-h-screen">
-                <PublicNavBar />
-                <div className="flex-grow flex flex-col justify-start items-center">
-                    {children}
-                </div>
-            </div>
-        </>
-    );
+function MainLayout({ isLoggedIn, children }) {
+  return (
+    <>
+      <div className = "flex flex-col min-h-screen">
+        {isLoggedIn === -1 ? (
+          <PublicNavBar />
+        ) : isLoggedIn === 0 ? (
+          <NavBar />
+        ) : (
+          <AdminNavBar />
+        )}
+        <div className = "flex-grow flex flex-col justify-start items-center">
+          {children}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MainLayout;
+
+// -1 : PUBLIC
+// 0  : PLAYER
+// 1  : ADMIN
