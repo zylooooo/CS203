fa4fca7ac5a652b4a4adc2cf1438340ded7cfa1a
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-09-26 19:55:07 +0800

    Standardized file naming for administrator

--- rally-rank-app/src/administrator/AdministratorIndex.jsx
+++ rally-rank-app/src/administrator/AdministratorIndex.jsx
@@ -1,6 +1,6 @@
-import AdministratorLogin from "./Login";
-import AdministratorTools from "./Tools";
-import AdministratorTournaments from "./Tournaments";
+import AdministratorLogin from "./AdministratorLogin";
+import AdministratorTools from "./AdministratorTools";
+import AdministratorTournaments from "./AdministratorTournaments";
 import AdministratorCreateTournaments from "./AdministratorCreateTournaments";
 
 export { AdministratorLogin, AdministratorTools, AdministratorTournaments, AdministratorCreateTournaments };
