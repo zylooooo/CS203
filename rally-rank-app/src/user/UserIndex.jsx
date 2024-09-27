aad847eb549283252bd1e44f7b09456bd33af023
f4heemmk.2023 <f4heemmmm@gmail.com>
2024-09-28 00:13:15 +0800

    Added import statements for User files and Public files, removed UserIndex.jsx and PublicIndex.jsx

--- rally-rank-app/src/user/UserIndex.jsx
+++
@@ -1,6 +0,0 @@
-import UserHome from "./UserHome";
-import UserLogin from "./UserLogin";
-import UserProfile from "./UserProfile";
-import UserTournaments from "./UserTournaments";
-
-export { UserHome, UserLogin, UserProfile, UserTournaments };
