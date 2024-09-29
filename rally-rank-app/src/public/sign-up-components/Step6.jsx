7dc9a15c855c951d37b3cddeb8693e838736b3a0
f4heemmk.2023 <f4heemmmm@gmail.com>
2024-09-29 11:13:51 +0800

    Edited Step4.jsx -> Allows for users to select blood type instead of typing down, allows users to select emergency contact relalationship OR type in another relationship if they choose 'Other'. Edited Step6.jsx -> added section for users to input username, performed further validation checks for password

--- rally-rank-app/src/public/sign-up-components/Step6.jsx
+++ rally-rank-app/src/public/sign-up-components/Step6.jsx
@@ -1,64 +1,111 @@
 import PropTypes from "prop-types";
 
-const Step6 = ({ register, errors, watch }) => (
-  <div>
-    <h2 className="text-xl font-extrabold">Account Setup</h2>
-    <p>Last stage! Please choose a password</p>
-    <div className="flex flex-col gap-5 mt-8">
-      <div className="flex flex-col gap-2">
-        <label
-          htmlFor="password"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Password
-        </label>
-        <input
-          className="border p-2"
-          type="password"
-          id="password"
-          placeholder="Enter your password"
-          {...register("password", {
-            required: "Password is required",
-            minLength: {
-              value: 8,
-              message: "Password must be at least 8 characters long",
-            },
-          })}
-        />
-        <p className="error">{errors.password?.message}</p>
-      </div>
+const Step6 = ({ register, errors, watch }) => {
 
-      <div className="flex flex-col gap-2">
-        <label
-          htmlFor="confirmPassword"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Confirm Password
-        </label>
-        <input
-          className="border p-2"
-          type="password"
-          id="confirmPassword"
-          placeholder="Confirm your password"
-          {...register("confirmPassword", {
-            required: "Password is required",
-            validate: (value) =>
-              value === watch("password") || "Passwords do not match",
-          })}
-        />
-        <p className="error">{errors.confirmPassword?.message}</p>
-      </div>
-    </div>
-  </div>
-);
+    const password = watch("password");
+
+    return (
+        <div>
+            <h2 className = "text-xl font-extrabold"> Account Setup </h2>
+            <p> Last stage of your account registration! Please enter a username and password </p>
+            <div className = "flex flex-col gap-5 mt-8">
+
+                {/* USERNAME INPUT */}
+                <div className = "flex flex-col gap-2">
+                    <label
+                        htmlFor = "username"
+                        className = "block text-sm font-medium text-gray-700"
+                    >
+                        Username
+                    </label>
+                    <input
+                        className = "border p-2"
+                        type = "text"
+                        id = "username"
+                        placeholder = "Enter your RallyRank username"
+                        {...register("username", {
+                            required: "A username is required.",
+                            minLength: {
+                                value: 5, 
+                                message: "Username must be at least 5 characters long",
+                            },
+                            maxLength: {
+                                value: 20,
+                                message: "Username cannot exceed 20 characters.",
+                            },
+                            pattern: {
+                                value: /^[a-zA-Z0-9_]*$/,
+                                message: "Username can only contain letters, numbers and underscores.",
+                            },
+                        })}
+                    />
+                    <p className = "error"> {errors.username?.message} </p>
+                </div>
+
+                {/* PASSWORD */}
+                <div className = "flex flex-col gap-2">
+                    <label
+                        htmlFor = "password"
+                        className = "block text-sm font-medium text-gray-700"
+                    >
+                        Password
+                    </label>
+                    <input
+                        className = "border p-2"
+                        type = "password"
+                        id = "password"
+                        placeholder = "Enter your password"
+                        {...register("password", {
+                            required: "A password is required.",
+                            minLength: {
+                                value: 8,
+                                message: "Password must be at least 8 characters long.",
+                            },
+                            validate: {
+                                alphanumeric: value => /^(?=.*[a-zA-Z])(?=.*[0-9])/.test(value) || "Password must be alphanumeric.",
+                                uppercase: value => /(?=.*[A-Z])/.test(value) || "Password must contain at least one uppercase letter.",
+                                lowercase: value => /(?=.*[a-z])/.test(value) || "Password must contain at least one lowercase letter.",
+                                symbol: value => /(?=.*[!@#$%^&*(),.?":{}|<>])/.test(value) || "Password must contain at least one special character.",
+                            },
+                        })}
+                    />
+                    <p className = "error"> {errors.password?.message} </p>
+                </div>
+
+                {/* CONFIRM PASSWORD */}
+                <div className = "flex flex-col gap-2">
+                    <label
+                        htmlFor = "confirmPassword"
+                        className = "block text-sm font-medium text-gray-700"
+                    >
+                        Confirm Password
+                    </label>
+                    <input
+                        className = "border p-2"
+                        type = "password"
+                        id = "confirmPassword"
+                        placeholder = "Re-enter your password"
+                        {...register("confirmPassword", {
+                            required: "Confirm password is required",
+                            validate: (value) =>
+                                value === password || "Passwords do not match!",
+                        })}
+                    />
+                    <p className = "error"> {errors.confirmPassword?.message} </p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 Step6.propTypes = {
-  register: PropTypes.func.isRequired,
-  errors: PropTypes.shape({
-    password: PropTypes.object,
-    confirmPassword: PropTypes.object,
-  }).isRequired,
-  watch: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    errors: PropTypes.shape({
+        username: PropTypes.object,
+        password: PropTypes.object,
+        confirmPassword: PropTypes.object,
+    }).isRequired,
+    watch: PropTypes.func.isRequired,
 };
 
 export default Step6;
