b7777c0a5a548be10c7486df34fdd7a27e3ad910
f4heemmk.2023 <f4heemmmm@gmail.com>
2024-10-09 23:47:47 +0800

    Cleaned up code for UserTournaments.jsx

--- rally-rank-app/src/user/UserTournaments.jsx
+++ rally-rank-app/src/user/UserTournaments.jsx
@@ -3,204 +3,201 @@ import { useNavigate } from "react-router-dom";
 import profilePictureTest1 from "../assets/profile-picture-one.jpg";
 import profilePictureTest2 from "../assets/profile-picture-two.jpg";
 
-// Replace with API call
+// ------------------------------------- API CALLS - MOCK PLAYER DATA ------------------------------------------------
+// API Calls - Mock tournament data (Upcoming)
 const upcomingTournaments = [
     {
-      name: "Tournament 3",
-      organizerProfilePicture: profilePictureTest1,
-      organizerName: "Puff Diddy",
-      startDate: "03/09/2024",
-      endDate: "17/09/2024",
-      eloRatingRange: "1200 - 1500",
-      venue: "Choa Chu Kang Stadium, Singapore 689236",
-      remarks: "",
+        name: "[Sample UpcomingTournament] Salibandy",
+        organizerProfilePicture: profilePictureTest1,
+        organizerName: "Puff Daddy",
+        startDate: "03/11/2024",
+        endDate: "17/11/2024",
+        minElo: "400",
+        maxElo: "500",
+        venue: "Choa Chu Kang Stadium, Singapore 238232",
+        remarks: "",
     },
     {
-      name: "Tournament 4",
-      organizerProfilePicture: profilePictureTest2,
-      organizerName: "Justin Beiber",
-      startDate: "03/09/2024",
-      endDate: "17/09/2024",
-      eloRatingRange: "1200 - 1500",    // change to minElo maxElo
-      venue: "Choa Chu Kang Stadium, Singapore 689236",
-      remarks: "This is only for the LGBTQ community",
+        name: "[Sample UpcomingTournament] Innerbandy",
+        organizerProfilePicture: profilePictureTest2,
+        organizerName: "Justin Bieber",
+        startDate: "07/12/2024",
+        endDate: "21/12/2024",
+        minElo: "500",
+        maxElo: "700",
+        venue: "Choa Chu Kang Stadium, Singapore 238232",
+        remarks: "Marc Wahlberg will be coming to watch the game.",
     },
-  ];
+];
 
-  // Replace with API call
+// API Calls - Mock tournament data (Past)
 const pastTournaments = [
     {
-      name: "Tournament 5",
-      organizerProfilePicture: profilePictureTest1,
-      organizerName: "Puff Diddy",
-      startDate: "03/09/2024",
-      endDate: "17/09/2024",
-      eloRatingRange: "1200 - 1500",
-      venue: "Choa Chu Kang Stadium, Singapore 689236",
-      remarks: "",
+        name: "[Sample PastTournament] Rookie Rumble",
+        organizerProfilePicture: profilePictureTest1,
+        organizerName: "Puff Daddy",
+        startDate: "03/04/2024",
+        endDate: "17/04/2024",
+        minElo: "400",
+        maxElo: "500",
+        venue: "SMU Tennis Court, Singapore 938923",
+        remarks: "",
     },
     {
-      name: "Tournament 6",
-      organizerProfilePicture: profilePictureTest2,
-      organizerName: "Justin Beiber",
-      startDate: "03/09/2024",
-      endDate: "17/09/2024",
-      eloRatingRange: "1200 - 1500",
-      venue: "Choa Chu Kang Stadium, Singapore 689236",
-      remarks: "",
+        name: "[Sample PastTournament] Taqwa",
+        organizerProfilePicture: profilePictureTest2,
+        organizerName: "Justin Bieber",
+        startDate: "07/02/2024",
+        endDate: "21/02/2024",
+        minElo: "1000",
+        maxElo: "1500",
+        venue: "Choa Chu Kang Stadium, Singapore 238232",
+        remarks: "",
     },
-  ];
+];
 
-  // Replace with API call
-  const myTournaments = [
+const myTournaments = [
     {
-      name: "Tournament 1",
-      organizerProfilePicture: profilePictureTest1,
-      organizerName: "Puff Diddy",
-      startDate: "03/09/2024",
-      endDate: "17/09/2024",
-      eloRatingRange: "1200 - 1500",
-      venue: "Choa Chu Kang Stadium, Singapore 689236",
-      remarks: "",
+        name: "[Sample MyTournament] Wonder Games",
+        organizerProfilePicture: profilePictureTest1,
+        organizerName: "Puff Daddy",
+        startDate: "03/04/2024",
+        endDate: "17/04/2024",
+        minElo: "400",
+        maxElo: "500",
+        venue: "SMU Tennis Court, Singapore 938923",
+        remarks: "",
     },
     {
-      name: "Tournament 2",
-      organizerProfilePicture: profilePictureTest2,
-      organizerName: "Justin Beiber",
-      startDate: "03/09/2024",
-      endDate: "17/09/2024",
-      eloRatingRange: "1200 - 1500",
-      venue: "Choa Chu Kang Stadium, Singapore 689236",
-      remarks: "",
+        name: "[Sample MyTournament] Orion Star",
+        organizerProfilePicture: profilePictureTest2,
+        organizerName: "Justin Bieber",
+        startDate: "07/05/2024",
+        endDate: "21/05/2024",
+        minElo: "1000",
+        maxElo: "1500",
+        venue: "Choa Chu Kang Stadium, Singapore 238232",
+        remarks: "",
     },
-  ];
+];
+// -------------------------------------------------------------------------------------------------------------------
 
-// Define the Tournaments component - format to display tournaments
-const Tournaments = ({ tournamentTest }) => {
+// Define the TournamentButtons component - Upcoming Tournaments, Past Tournaments and My Tournaments
+const TournamentButtons = ({ buttons, onUpcomingTournamentsClick, onPastTournamentsClick, onMyTournamentsClick}) => {
+    const [activeButton, setActiveButton] = useState(0)     // "Upcoming Tournaments" button will be the active button
+
+    const handleButtonClick = (index) => {
+        setActiveButton(index);
+        if (index === 0) {
+            onUpcomingTournamentsClick();
+        } else if (index === 1) {
+            onPastTournamentsClick();
+        } else {
+            onMyTournamentsClick();
+        }
+    };
+
+    return (
+        <div className = "tournament-buttons flex gap-5">
+            {buttons.map((buttonLabel, index) => (
+                <button
+                    key = {index}
+                    className = {`btn transition-colors duration-300 ${activeButton === index ? 'active-button underline' : 'text-gray-700 hover:text-blue-500 hover:text-red-500'}`}
+                    onClick = {() => handleButtonClick(index)}>
+                        {buttonLabel}
+                    </button>
+            ))}
+        </div>
+    );
+}
+
+// Define the TournamentCard component - Template to display the tournaments
+const TournamentCard = ({ tournamentType }) => {
     const navigate = useNavigate();
 
     const handleTournamentCardClick = (tournament) => {
-        navigate("/tournament-details", {state: tournament });  // redirect with tournament data
+        navigate("/tournament-details", {state: tournament});
     };
 
-  return (
-    <div className="tournaments-list flex flex-col space-y-8">
-      {tournamentTest.map((tournament, index) => (
-        <div
-          key={index}
-          className="tournament border rounded-lg p-4 bg-white shadow-md cursor-pointer hover:shadow-lg transition flex w-4/5"
-          onClick={() => handleTournamentCardClick(tournament)}
-        >
-          <div className="flex-1 pr-4">
-            <h3 className="section-one text-xl font-bold mb-2">
-              {tournament.name}
-            </h3>
-            <div className="tournament-organiser-details flex items-center mb-2">
-              <img
-                src={tournament.organizerProfilePicture}
-                alt="Organizer"
-                className="organiser-picture w-8 h-8 rounded-full mr-2"
-              />
-              <p className="organiser-name text-gray-600">
-                Organiser: {tournament.organizerName}
-              </p>
-            </div>
-            <p className="tournament-date text-gray-500 mb-2">
-              Date: {tournament.startDate} to {tournament.endDate}
-            </p>
-            <p className="tournament-elo-rating-range text-gray-500">
-              Elo Rating: {tournament.eloRatingRange}
-            </p>
-          </div>
-          <div className="section-two border-l border-gray-300 pl-4 flex-none w-1/3">
-            <p className="text-gray-600 font-semibold">Venue</p>
-            <p className="text-gray-500">{tournament.venue}</p>
-          </div>
+    return (
+        <div className = "tournaments-cards-list flex flex-col space-y-8">
+            {tournamentType.map((tournament, index) => (
+                <div
+                    key = {index}
+                    className = "tournament-card border rounded-lg p-4 bg-white shadow-md cursor-pointer hover:shadow-lg transition flex w-4/5"
+                    onClick = {() => handleTournamentCardClick(tournament)}
+                >
+                    <div className = "card-section-one flex-1 pr-4">
+                        <h3 className = "text-xl font-bold mb-2"> {tournament.name} </h3>
+                        <div className = "flex items-center mb-2">
+                            <img
+                                src = {tournament.organizerProfilePicture}
+                                alt = {tournament.organizerName}
+                                className = "organizer-picture w-8 h-8 rounded-full mr-2"
+                            />
+                            <p> Organiser: {tournament.organizerName} </p>
+                        </div>
+                        <p className = "mb-2"> Date: {tournament.startDate} to {tournament.endDate} </p>
+                        <p> Elo Rating: {tournament.minElo} to {tournament.maxElo} </p>
+                    </div>
+                    <div className = "card-section-two border-l pl-4 flex-none w-1/3">
+                        <p className = "font-semibold"> Venue: </p>
+                        <p> {tournament.venue} </p>
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
-// Define the TournamentsButtons component
-const TournamentsButtons = ({ buttons, onUpcomingClick, onPastClick, onMyClick }) => {
-  const [activeButton, setActiveButton] = useState(0); // "Upcoming Button" will be the first active button
-
-  const handleButtonClick = (index) => {
-    setActiveButton(index);
-    if (index === 0) {
-      onUpcomingClick();
-    } else if (index === 1) {
-      onPastClick();
-    } else {
-      onMyClick();
-    }
-  };
-
-  return (
-      <div className = "tournaments-buttons flex gap-5">
-          {buttons.map((buttonLabel, index) => (
-              <button 
-                  key = {index}
-                  className = {`btn transition-colors duration-300 ${activeButton === index ? 'active-button underline' : 'text-gray-700 hover:text-blue-500 hover:text-red-500'}`} 
-                  onClick = {() => handleButtonClick(index)}>
-                      {buttonLabel}
-              </button>
-          ))}
-      </div>
-  );
-}
-
-// Define the UserTournaments component
 function UserTournaments() {
-  const [tournamentTest, setTournamentTest] = useState(upcomingTournaments);
+    const [tournamentType, setTournamentType] = useState(upcomingTournaments)   // Consistent with Line 87
 
-  const handleUpcomingClick = () => {
-    setTournamentTest(upcomingTournaments);
-  };
+    const handleUpcomingTournamentsClick = () => {
+        setTournamentType(upcomingTournaments);
+    };
 
-  const handlePastClick = () => {
-    setTournamentTest(pastTournaments);
-  };
+    const handlePastTournamentsClick = () => {
+        setTournamentType(pastTournaments);
+    };
 
-  const handleMyClick = () => {
-    setTournamentTest(myTournaments);
-  };
+    const handleMyTournamentsClick = () => {
+        setTournamentType(myTournaments);
+    };
 
-  return (
-    <>
-      <div className = "tournaments-page main-container">
-        <div className = "row-container flex flex-col w-3/5 gap-8">
+    return (
+        <>
+            <div className = "user-tournaments-page main-container">
+                <div className = "row-container flex flex-col w-3/5 gap-8">
 
-          {/* LABELS */}
-          <TournamentsButtons
-              buttons={["Upcoming Tournaments", "Past Tournaments", "My Tournaments"]}
-              onUpcomingClick={handleUpcomingClick}
-              onPastClick={handlePastClick}
-              onMyClick={handleMyClick}
-            />
+                    {/* TOURNAMENT BUTTONS: UPCOMING, PAST, MY */}
+                    <TournamentButtons
+                        buttons = {["Upcoming Tournaments", "Past Tournaments", "My Tournaments"]}
+                        onUpcomingTournamentsClick = {handleUpcomingTournamentsClick}
+                        onPastTournamentsClick = {handlePastTournamentsClick}
+                        onMyTournamentsClick = {handleMyTournamentsClick}
+                    />
 
+                    {/* SEARCH BAR */}
+                    <div className = "tournament-search-bar flex gap-3">
+                        <input
+                            className = "border rounded p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            type = "text"
+                            placeholder = "Search for tournaments..."
+                        />
+                        <button className = "border border-blue-500 text-blue-500 rounded-xl px-4 py-2 hover:bg-blue-500 hover:text-white transition">
+                            Search
+                        </button>
+                    </div>
 
-          {/* SEARCH BAR */}
-          <div className = "tournaments-search-bar flex gap-3">
-            <input
-              className = "border rounded p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-              type="text"
-              placeholder = "Search tournaments..."
-            />
-            <button className = "border border-blue-500 text-blue-500 rounded-xl px-4 py-2 hover:bg-blue-500 hover:text-white transition">
-              Search
-            </button>
-          </div>
+                    {/* LIST OF TOURNAMENT CARDS */}
+                    <TournamentCard tournamentType = {tournamentType} />
+                </div>
 
-          {/* TOURNAMENTS LIST */}
-          <Tournaments tournamentTest={tournamentTest} />
-        </div>
-
-        <div className = "col-container">Ongoing Tournaments</div>
-      </div>
-    </>
-  );
+                <div className = "col-container"> Ongoing Tournaments </div>
+            </div>
+        </>
+    );
 };
 
 export default UserTournaments;
