42932f0ef3258fc4e7c864c3e4294e2cc4002cb3
Tori <tori.ng.2023@scis.smu.edu.sg>
2024-10-11 13:19:24 +0800

    Edited AdministratorTools.jsx, added View Strike and Issue Strike buttons

--- rally-rank-app/tailwind.config.js
+++ rally-rank-app/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
             "primary-color-white": "#FFFFFF",
             "primary-color-black": "#262626",
             "secondary-color-dark-green": "#10574E",
-            "secondary-color-light-gray": "#525252",
+            "secondary-color-light-gray": "#A6A6A6",
             "secondary-color-light-green": "#CDF731",
             "red-color": "#FF0000",
         },
