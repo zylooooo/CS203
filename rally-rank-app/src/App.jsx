502244c722c65d6022331ed00abaa0233a99f6ad
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-10-13 17:28:35 +0800

    User login now stores username, role, and jwtToken to localStorage

--- rally-rank-app/src/App.jsx
+++ rally-rank-app/src/App.jsx
@@ -92,7 +92,7 @@ function App() {
 
                     {/* Protected Player Routes */}
                     <Route
-                        path="/home"
+                        path="/users/home"
                         element={
                             <PrivateRoute>
                                 <MainLayout>
