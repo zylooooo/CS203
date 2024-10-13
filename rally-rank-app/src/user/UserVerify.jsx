45fce9b0764df1b4d9d6ec0307e54de754b882e2
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-10-13 20:33:02 +0800

    Edited Resend verification code but incomplete

--- rally-rank-app/src/user/UserVerify.jsx
+++ rally-rank-app/src/user/UserVerify.jsx
@@ -11,7 +11,32 @@ import { useForm } from "react-hook-form";
 // Authentication imports
 import { useAuth } from "../authentication/AuthContext"; // Import the AuthContext
 
-async function resendCode(username) {}
+async function resendCode(username, setResendError) {
+    try {
+        const response = await axios.post(
+            "http://localhost:8080/auth/user-resend",
+            { username },
+            { withCredentials: true }
+        );
+        if (response.status === 200) {
+            setResendError("Verification code resent successfully!");
+        }
+    } catch (error) {
+        const status = error.status;
+
+        if (status === 400) {
+            setResendError("User is already verified!");
+        } else if (status === 404) {
+            setResendError("User is not found!");
+        } else if (status === 500) {
+            setResendError(
+                "Internal server error. Please try again or contact support."
+            );
+        } else {
+            setResendError("Failed to resend code. Please try again.");
+        }
+    }
+}
 
 function UserVerify() {
     const form = useForm();
@@ -20,6 +45,11 @@ function UserVerify() {
     const { loginUser } = useAuth(); // Destructure loginUser from AuthContext
     const navigate = useNavigate(); // For navigation after verification
     const [verifyError, setVerifyError] = useState(""); // State for handling verification errors
+
+    // State to manage modal visibility
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [resendEmail, setResendEmail] = useState(""); // Email for resending the verification code
+    const [resendError, setResendError] = useState(""); // State for handling resend errors
 
     // Function to verify user with username and OTP code
     async function verifyUser(username, verificationCode) {
@@ -56,7 +86,10 @@ function UserVerify() {
 
     const onSubmit = async (formData) => {
         // Call the verifyUser function with username and otpCode
-        const response = await verifyUser(formData.username, formData.otpCode);
+        const response = await verifyUser(
+            formData.username,
+            formData.verificationCode
+        );
 
         if (response !== undefined) {
             // Re-route to home after successful verification
@@ -64,94 +97,157 @@ function UserVerify() {
         }
     };
 
+    // Handle modal form submit to resend code
+    const handleResendSubmit = async (event) => {
+        event.preventDefault();
+        await resendCode(resendEmail, setResendError); // Pass setResendError to update error state
+    };
+
     return (
         <>
-            <div
-                className="bg-cover bg-center h-screen-minus-navbar w-screen flex flex-col justify-center items-center"
-                style={{ backgroundImage: `url(${loginBackground})` }}
-            >
-                <div className="card rounded-none bg-primary-color-white border-none items-center m-8">
-                    <h1 className="font-bold text-2xl bg-special-blue">
-                        User Verification
-                    </h1>
-                    <form
-                        className="card px-0 py-4 border-none shadow-none bg-primary-color-white"
-                        onSubmit={handleSubmit(onSubmit)}
-                        noValidate
-                    >
-                        <div>
-                            <label
-                                htmlFor="username"
-                                className="block text-sm font-medium text-gray-700"
-                            >
-                                Username
-                            </label>
-                            <input
-                                className="input"
-                                type="text"
-                                id="username"
-                                placeholder="Username"
-                                {...register("username", {
-                                    required: "Username is required",
-                                })}
-                            />
-                            <p className="error">{errors.username?.message}</p>
-                        </div>
-
-                        <div>
-                            <label
-                                htmlFor="verificationCode"
-                                className="block text-sm font-medium text-gray-700"
-                            >
-                                OTP Code
-                            </label>
-                            <input
-                                type="text"
-                                id="verificationCode"
-                                className="input"
-                                placeholder="Enter OTP Code"
-                                {...register("verificationCode", {
-                                    required: "Verification code is required",
-                                })}
-                            />
-                            <p className="error">
-                                {errors.verificationCode?.message}
-                            </p>
-                        </div>
-
-                        {verifyError && (
-                            <p className="error text-red-500 mt-2">
-                                {verifyError}
-                            </p>
-                        )}
-
-                        <button
-                            type="submit"
-                            className="button mt-6 font-bold hover:shadow-inner"
+            {!isModalOpen && (
+                <div
+                    className="bg-cover bg-center h-screen-minus-navbar w-screen flex flex-col justify-center items-center"
+                    style={{ backgroundImage: `url(${loginBackground})` }}
+                >
+                    <div className="card rounded-none bg-primary-color-white border-none items-center m-8">
+                        <h1 className="font-bold text-2xl bg-special-blue">
+                            User Verification
+                        </h1>
+                        <form
+                            className="card px-0 py-4 border-none shadow-none bg-primary-color-white"
+                            onSubmit={handleSubmit(onSubmit)}
+                            noValidate
                         >
-                            Verify
-                        </button>
+                            <div>
+                                <label
+                                    htmlFor="username"
+                                    className="block text-sm font-medium text-gray-700"
+                                >
+                                    Username
+                                </label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    id="username"
+                                    placeholder="Username"
+                                    {...register("username", {
+                                        required: "Username is required",
+                                    })}
+                                />
+                                <p className="error">
+                                    {errors.username?.message}
+                                </p>
+                            </div>
 
+                            <div>
+                                <label
+                                    htmlFor="verificationCode"
+                                    className="block text-sm font-medium text-gray-700"
+                                >
+                                    OTP Code
+                                </label>
+                                <input
+                                    type="text"
+                                    id="verificationCode"
+                                    className="input"
+                                    placeholder="Enter OTP Code"
+                                    {...register("verificationCode", {
+                                        required:
+                                            "Verification code is required",
+                                    })}
+                                />
+                                <p className="error">
+                                    {errors.verificationCode?.message}
+                                </p>
+                            </div>
+
+                            {verifyError && (
+                                <p className="error text-red-500 mt-2">
+                                    {verifyError}
+                                </p>
+                            )}
+
+                            <button
+                                type="submit"
+                                className="button mt-6 font-bold hover:shadow-inner"
+                            >
+                                Verify
+                            </button>
+                        </form>
                         <button
                             className="text-xs underline text-secondary-color-dark-green text-center pt-5 hover:text-primary-color-green"
-                            onClick={resendCode}
+                            onClick={() => setModalOpen(true)} // Open modal on click
                         >
                             Resend verification code
                         </button>
-                    </form>
-                </div>
-                <div className="card p-7 rounded-none bg-primary-color-white border-none items-center">
-                    <div className="text-ms flex flex-row justify-center align-item">
-                        Already verified?
-                        <Link
-                            to="/auth/user-login"
-                            className="hover:text-primary-color-green font-bold underline pl-2 text-secondary-color-dark-green"
-                        >
-                            Log in here!
-                        </Link>
+                    </div>
+                    <div className="card p-7 rounded-none bg-primary-color-white border-none items-center">
+                        <div className="text-ms flex flex-row justify-center align-item">
+                            Already verified?
+                            <Link
+                                to="/auth/user-login"
+                                className="hover:text-primary-color-green font-bold underline pl-2 text-secondary-color-dark-green"
+                            >
+                                Log in here!
+                            </Link>
+                        </div>
                     </div>
                 </div>
-            </div>
+            )}
+
+            {/* Modal for resending code */}
+            {isModalOpen && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+                    <div className="bg-white p-6 rounded-md shadow-lg">
+                        <div className="flex justify-between mb-4">
+                            <h2 className="text-xl font-bold">
+                                Resend Verification Code
+                            </h2>
+                            <button
+                                type="button"
+                                className="border p-1"
+                                onClick={() => setModalOpen(false)} // Close modal on cancel
+                            >
+                                X
+                            </button>
+                        </div>
+                        <form onSubmit={handleResendSubmit}>
+                            <div>
+                                <label
+                                    htmlFor="resendEmail"
+                                    className="block text-sm font-medium text-gray-700"
+                                >
+                                    Enter your email to resend the code
+                                </label>
+                                <input
+                                    type="text"
+                                    id="resendEmail"
+                                    className="input"
+                                    placeholder="Your email"
+                                    value={resendEmail}
+                                    onChange={(e) =>
+                                        setResendEmail(e.target.value)
+                                    } // Update email state
+                                    required
+                                />
+
+                                {resendError && (
+                                    <p className="error text-red-500 mt-2">
+                                        {resendError}
+                                    </p>
+                                )}
+                            </div>
+                            <button
+                                type="submit"
+                                className="button mt-4 font-bold hover:shadow-inner"
+                            >
+                                Resend Code
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
