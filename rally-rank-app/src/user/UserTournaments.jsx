3d8424c4d0f1922c3f0dcb88d11f5b5dc7531dc4
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-10-15 21:30:25 +0800

    getUsersTournamentSchedules() now GET from backend

--- rally-rank-app/src/user/UserTournaments.jsx
+++ rally-rank-app/src/user/UserTournaments.jsx
@@ -181,7 +181,7 @@ function UserTournaments() {
             }
 
             const response = await axios.get(
-                "http://localhost:8080/auth/usersTournaments/scheduled",
+                "http://localhost:8080/users/tournaments/scheduled",
                 {
                     withCredentials: true,
                     headers: {
