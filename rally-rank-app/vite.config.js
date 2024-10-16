c92ae5bf827f084f1a497539d8da2a2e172dab10
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-10-16 18:43:59 +0800

    Edited vite.config.js for aws amplify

--- rally-rank-app/vite.config.js
+++ rally-rank-app/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/',  // Ensure this is correct for AWS Amplify
+  build: {
+    outDir: 'dist',
+  },
+});
