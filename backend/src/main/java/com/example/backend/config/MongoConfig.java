99496b39c23af3b405c3597a2baaa57ea2beb951
Joshua <djcheung.2023@scis.smu.edu.sg>
2024-10-23 09:38:19 +0800

    BugFix: MongoConfig
    - fixed a nasty bug where the createdAt and updatedAt fields will be stored as strings in the database. Removed the converter for that. Seems to work

--- backend/src/main/java/com/example/backend/config/MongoConfig.java
+++ backend/src/main/java/com/example/backend/config/MongoConfig.java
@@ -19,7 +19,7 @@ import java.util.Arrays;
 @Configuration
 public class MongoConfig {
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    // private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     @Bean
     public MongoCustomConversions customConversions() {
@@ -27,7 +27,7 @@ public class MongoConfig {
         converters.add(new LocalDateToDateConverter());
         converters.add(new DateToLocalDateConverter());
         converters.add(new StringToLocalDateTimeConverter());
-        converters.add(new LocalDateTimeToStringConverter());
+        // converters.add(new LocalDateTimeToStringConverter());
         converters.add(new LocalDateToStringConverter());
         converters.add(new StringToLocalDateConverter());
         return new MongoCustomConversions(converters);
@@ -77,12 +77,12 @@ public class MongoConfig {
     }
 }
 
-    static class LocalDateTimeToStringConverter implements Converter<LocalDateTime, String> {
-        @Override
-        public String convert(@NonNull LocalDateTime source) {
-            return source.format(DATE_TIME_FORMATTER);
-        }
-    }
+    // static class LocalDateTimeToStringConverter implements Converter<LocalDateTime, String> {
+    //     @Override
+    //     public String convert(@NonNull LocalDateTime source) {
+    //         return source.format(DATE_TIME_FORMATTER);
+    //     }
+    // }
 
     static class LocalDateToStringConverter implements Converter<LocalDate, String> {
         @Override
