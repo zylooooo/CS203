780d23a6838fad904491d23fc7012067b56a2a44
Joshua <djcheung.2023@scis.smu.edu.sg>
2024-10-23 16:53:12 +0800

    Added 4 new Admin Tournament Controllers
    ADMIN
    tournaments page:
    - all tournaments: all ongoing and future tournaments in the database (/admins/tournaments/ongoing)
    - my tournaments: all ongoing and future tournaments that I’ve created (/admins/tournaments/scheduled)
    
    tournament history page:
    - all past tournaments: history of all tournaments (/admins/tournaments/all-history)
    - all my past tournaments: history of tournaments for specific admin (/admins/tournaments/my-history)

--- backend/src/test/java/com/example/backend/model/TournamentTest.java
+++ backend/src/test/java/com/example/backend/model/TournamentTest.java
@@ -2,7 +2,6 @@ package com.example.backend.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
