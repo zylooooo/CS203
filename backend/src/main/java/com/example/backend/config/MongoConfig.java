39b0e8eb28d82f92bff3da96206c71b6eb85f7e4
zhi yi <zhiyi.loo.2023@scis.smu.edu.sg>
2024-10-23 19:56:51 +0800

    updated the mongoConfig

--- backend/src/main/java/com/example/backend/config/MongoConfig.java
+++ backend/src/main/java/com/example/backend/config/MongoConfig.java
@@ -19,8 +19,6 @@ import java.util.Arrays;
 @Configuration
 public class MongoConfig {
 
-    // private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-
     @Bean
     public MongoCustomConversions customConversions() {
         List<Converter<?, ?>> converters = new ArrayList<>();
@@ -47,13 +45,6 @@ public class MongoConfig {
         }
     }
 
-    // static class StringToLocalDateTimeConverter implements Converter<String, LocalDateTime> {
-    //     @Override
-    //     public LocalDateTime convert(@NonNull String source) {
-    //         return LocalDateTime.parse(source, DATE_TIME_FORMATTER);
-    //     }
-    // }
-
     public class StringToLocalDateTimeConverter implements Converter<String, LocalDateTime> {
     private static final List<DateTimeFormatter> formatters = Arrays.asList(
         DateTimeFormatter.ofPattern("yyyy-M-d HH:mm"),
@@ -75,14 +66,9 @@ public class MongoConfig {
         }
         throw new IllegalArgumentException("Unable to parse datetime: " + source);
     }
-}
+    }
 
-    // static class LocalDateTimeToStringConverter implements Converter<LocalDateTime, String> {
-    //     @Override
-    //     public String convert(@NonNull LocalDateTime source) {
-    //         return source.format(DATE_TIME_FORMATTER);
-    //     }
-    // }
+
 
     static class LocalDateToStringConverter implements Converter<LocalDate, String> {
         @Override
