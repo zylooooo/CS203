c5ac8c6e41a3aa42f404ac0c78d6cd8d54c7e4f5
f4heemmk.2023 <f4heemmmm@gmail.com>
2024-10-25 03:34:11 +0800

    Cleaned code main.jsx

--- rally-rank-app/src/main.jsx
+++ rally-rank-app/src/main.jsx
@@ -1,5 +1,8 @@
+// Package Imports
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+// File Imports
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
