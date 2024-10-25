0fa82fe0c232be3b6a98596ed98cb7bbb114057b
Joshua <djcheung.2023@scis.smu.edu.sg>
2024-10-25 16:05:12 +0800

    Added /admins/tournaments/{tournamentName}/available-users
    Added the controller and service methods to allow admins to retrieve the list of available players that can partake in their current or future tournaments

--- backend/src/main/java/com/example/backend/model/User.java
+++ backend/src/main/java/com/example/backend/model/User.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import lombok.AllArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -21,7 +22,8 @@ import jakarta.validation.constraints.*;
 @Document(collection = "user")
 public class User {
 
-    private final int BASE_ELO = 1200;
+    @Transient
+    private static final int BASE_ELO = 1200;
 
     @Id
     private String id;
@@ -41,7 +43,6 @@ public class User {
     @Pattern(regexp = "^(?:6\\d{7}|[89]\\d{7}|1800\\d{7}|1900\\d{7})$", message = "Invalid phone number!") 
     private String phoneNumber;
 
-    // Default elo is 400
     private int elo = BASE_ELO;
 
     @NotBlank(message = "Gender is required!")
