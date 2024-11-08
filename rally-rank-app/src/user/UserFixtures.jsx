d67065905b5d52214f2b1c7c5bd925e70c9334ca
ashleytkl <johndoe@example.com>
2024-11-08 23:05:26 +0800

    updated userfixtures

--- rally-rank-app/src/user/UserFixtures.jsx
+++ rally-rank-app/src/user/UserFixtures.jsx
@@ -4,15 +4,23 @@ import { useState, useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import { Bracket, Seed, SeedItem, SeedTeam, SeedTime } from "react-brackets";
 
+// Assets and Components Imports
+import AlertMessageWarning from "../components/AlertMessageWarning";
+import AlertMessageSuccess from "../components/AlertMessageSuccess";
+
 // Icons Imports
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+import MatchResultsCard from "./components/MatchResultsCard";
+import WinnersTable from "./components/WinnersTable";
+import PreliminaryPlayersTable from "./components/PreliminaryPlayersTable";
 
 function UserFixtures() {
     const location = useLocation();
     const tournamentName = location.state?.tournamentName;
 
-    // Const: Hold reference for scrolling
+    // Const: Hold referance for scrolling
     const mainFixturesRef = useRef(null);
 
     // Consts: For determining the dropdown for the important rounds (QF, SF, F)
@@ -41,6 +49,10 @@ function UserFixtures() {
     const [semiFinalMatches, setSemiFinalMatches] = useState([]);
     const [quarterFinalMatches, setQuarterFinalsMatches] = useState([]);
 
+    // Consts to hold updating match-related data
+    const [currentMatch, setCurrentMatch] = useState({});
+    const [showMatchResultsCard, setShowMatchResultsCard] = useState(false);
+
     // Function: Format Date for easy readability
     const formatDate = (dateString) => {
         if (dateString === null) {
@@ -48,6 +60,24 @@ function UserFixtures() {
         }
         const date = new Date(dateString);
         return `${date.toLocaleString('en-US', { day: '2-digit' })} ${date.toLocaleString('en-US', { month: 'long' })} ${date.toLocaleString('en-US', { year: 'numeric' })}`;
+    };
+
+    // Function: Handle click for each seed
+    const handleClick = (id) => {
+        for (let i = 0; i < mainMatches.length; i++) {
+            for (let j = 0; j < mainMatches[i].matches.length; j++) {
+                if (mainMatches[i].matches[j].id === id) {
+                    handleMatchClick(mainMatches[i].matches[j]);
+                }
+            }
+        }
+    };
+
+    const handleMatchClick = (match) => {
+        setCurrentMatch(match);
+        if (match.startDate !== null && match.matchWinner !== null) {
+            setShowMatchResultsCard(true);
+        }
     };
 
     const scrollToMainFixtures = () => {
@@ -60,7 +90,7 @@ function UserFixtures() {
     const CustomSeed = ({ seed, breakpoint }) => {
         return (
             <Seed mobileBreakpoint = {breakpoint} style = {{ fontSize: "12px" }}>
-                <SeedItem style = {{ backgroundColor: "#E7F5E8", padding: "10px", borderRadius: "12px" }}>
+                <SeedItem onClick = {() => handleClick(seed.id)} style = {{ backgroundColor: "#E7F5E8", padding: "10px", borderRadius: "12px" }}>
                     <div>
                         <SeedTeam style = {{ color: "#444444", fontWeight: "700", fontSize: "14px"}}>
                             {seed.teams[0]?.name || "TBD"}
@@ -83,7 +113,7 @@ function UserFixtures() {
         try {
             const userData = JSON.parse(localStorage.getItem("userData"));
             if (!userData || !userData.jwtToken) {
-                console.error("No JWT token found");
+                console.error("No JWT Token found!");
                 return;
             }
 
@@ -97,22 +127,20 @@ function UserFixtures() {
                 }
             );
 
+            // Warning Message Alerts
             if (response.data.error !== undefined) {
                 console.log("error: ", response.data);
             }
-
             if (response.data.rounds.length !== 0) {
                 setTournamentBracket(response.data);
             } 
-
             if (response.data.rounds[0].matches.length > 0) {
                 setPreliminaryMatches(response.data.rounds[0].matches);
             }
-
             if (response.data.rounds.length > 1) {
                 setMainMatches(response.data.rounds.slice(1));
                 const roundsArr = [];
-                const currentFixtures = response.data.rounds.slice(1);
+                const currentFixtures = response.data.rounds.slice(1);  // starts from the index 1 - main match
                 for (let roundIndex = 0; roundIndex < currentFixtures.length; roundIndex++) {
                     const seeds = [];
                     const matchesPerRound = currentFixtures[roundIndex]?.matches;
@@ -121,11 +149,12 @@ function UserFixtures() {
                         roundTitle = "Quarter Finals";
                         setQuarterFinalsWinners(matchesPerRound.map(match => match.matchWinner));
                         setQuarterFinalsMatches(currentFixtures[roundIndex].matches);
+                        console.log("q finals: ", currentFixtures[roundIndex]);
                     } else if (matchesPerRound.length === 2) {
                         roundTitle = "Semi Finals";
-                        setSemiFinalsWinners(matchesPerRound.map(match => match.matchWinner));
-                        setSemiFinalMatches(currentFixtures[roundIndex].matches);
-                    } else if (matchesPerRound.length === 1 && roundIndex !== 0) {
+                            setSemiFinalsWinners(matchesPerRound.map(match => match.matchWinner));
+                            setSemiFinalMatches(currentFixtures[roundIndex].matches);
+                    } else if (matchesPerRound.length === 1 && roundIndex != 0) {
                         roundTitle = "Finals";
                         setFinalsMatch(currentFixtures[roundIndex].matches);
                     }
@@ -156,7 +185,9 @@ function UserFixtures() {
             return response;
 
         } catch (error) {
-            console.error('Error fetching tournament brackets data:', error.message);
+            // Warning Message Alert: Error fetching tournament brackets data
+            const errorMessage = error.response?.data?.error || 'An unknown error occurred';
+            console.error('Error fetching match results:', errorMessage);
         }
     };
 
@@ -166,80 +197,96 @@ function UserFixtures() {
     }, []);
 
     return (
-        <div className = "user-fixtures flex flex-col gap-8 p-9">
-            <button
-                onClick = {scrollToMainFixtures}
-                className = {`scroll-button p-2 text-white rounded-lg shadow-md mb-4 ${!tournamentBracket ? 'bg-gray-400 cursor-not-allowed' : 'bg-gray-600'}`}
-                style = {{ backgroundColor: !tournamentBracket ? 'gray' : 'grey' }}
-                disabled = {!tournamentBracket}
-            >
-                Go to Main Tournament Fixtures
-            </button>
-            <div className = "flex flex-col items-center gap-4">
-                {tournamentBracket === null && (
-                    <p className = "font-bold text-xl"> Preliminary Rounds and Main Tournament Fixtures have not been generated yet. </p>
-                )}
-            </div>
-            <button className = "text-2xl font-bold shadow-lg p-2 rounded-[12px]" onClick = {toggleQuarterFinals}>
-                View Quarter Finals
-                <span className = "text-lg ml-2">
-                    <FontAwesomeIcon icon = {showQuarterFinals ? faChevronUp : faChevronDown} />
-                </span>
-            </button>
-            {showQuarterFinals && quarterFinalsWinners.length > 0 && quarterFinalMatches.length > 0 && (
-                <div className = "flex-1 shadow-xl rounded-[12px]">
-                    {/* Display quarter-final matches */}
-                    {/* Example Component: Use your existing table or component structure */}
-                </div>
-            )}
-            <button className = "text-2xl font-bold shadow-lg p-2 rounded-[12px]" onClick = {toggleSemiFinals}>
-                View Semi Finals
-                <span className = "text-lg ml-2">
-                    <FontAwesomeIcon icon = {showSemiFinals ? faChevronUp : faChevronDown} />
-                </span>
-            </button>
-            {showSemiFinals && semiFinalsWinners.length > 0 && semiFinalMatches.length > 0 && (
-                <div className="flex-1 shadow-xl rounded-[12px]">
-                    {/* Display semi-final matches */}
-                    {/* Example Component: Use your existing table or component structure */}
-                </div>
-            )}
-
-            <button className = "text-2xl font-bold shadow-lg p-2 rounded-[12px]" onClick = {toggleFinals}>
-                View Finals
-                <span className="text-lg ml-2">
-                    <FontAwesomeIcon icon = {showFinals ? faChevronUp : faChevronDown} />
-                </span>
-            </button>
-            {showFinals && (
-                <>
-                    {finalsMatch && finalsMatch.length > 0 ? (
-                        <div className = "flex-1">
-                            {/* Display finals match */}
-                            {/* Example Component: Use your existing table or component structure */}
-                        </div>
-                    ) : (
-                        <div className = "flex-1 p-4 text-center text-lg font-semibold text-gray-500">
-                            No Finals matches available.
+        <>
+            <div className = "user-fixtures flex flex-row gap-8 p-9">
+                <div className = "w-full">
+                    <button
+                        onClick = {scrollToMainFixtures}
+                        className = {`scroll-button p-2 text-white rounded-lg shadow-md mb-6 w-full font-semibold ${!tournamentBracket ? 'bg-gray-400 cursor-not-allowed' : 'bg-gray-600'}`}
+                        style = {{ backgroundColor: !tournamentBracket ? 'gray' : 'grey' }}
+                        disabled = {!tournamentBracket}
+                    >
+                        Go to Main Tournament Fixtures
+                    </button>
+                    <div className = "flex flex-col items-center gap-4">
+                        {tournamentBracket === null && (
+                            <p className = "font-bold text-xl"> Preliminary Rounds and Main Tournament Fixtures has not been generated yet. </p>
+                        )}
+                    </div>
+                    <div className = "mb-12">
+                        {preliminaryMatches.length > 0 && (
+                            <PreliminaryPlayersTable preliminaryMatches = {preliminaryMatches} handleMatchClick = {handleMatchClick} />
+                        )}
+                    </div>
+                    {mainMatches.length > 0 && tournamentBracket !== null && (
+                        <div ref = {mainFixturesRef} className = "main-tournament-brackets mb-20">
+                            <h2 className = "text-2xl font-bold mb-10"> Main Tournament Fixtures and Results </h2>
+                            <Bracket
+                                rounds = {mainTournamentRounds}
+                                roundTitleComponent = {(title) => (
+                                    <div style = {{ textAlign: "center", color: "green", fontWeight: "600", fontSize: "18px" }}>
+                                        {title}
+                                    </div>
+                                )}
+                                renderSeedComponent = {CustomSeed}
+                            />
                         </div>
                     )}
-                </>
-            )}
-            {mainMatches.length > 0 && tournamentBracket !== null && (
-                <div ref = {mainFixturesRef} className = "main-tournament-brackets mb-20">
-                    <h2 className = "text-2xl font-bold mb-10"> Main Tournament Fixtures and Results </h2>
-                    <Bracket
-                        rounds = {mainTournamentRounds}
-                        roundTitleComponent = {(title) => (
-                            <div style = {{ textAlign: "center", color: "green", fontWeight: "600", fontSize: "18px" }}>
-                                {title}
+                    {showMatchResultsCard && (
+                        <MatchResultsCard matchDetails = {currentMatch} setShowMatchResultsCard = {setShowMatchResultsCard}/>
+                    )}
+                </div>
+                <div className = "flex flex-col w-1/3 gap-5 mt-12">
+                    <div className = "flex flex-col">
+                        <button className = "text-xl font-bold shadow-lg p-2 rounded-[12px]" onClick = {toggleQuarterFinals}>
+                            View Quarter Finals
+                            <span className = "text-lg ml-2">
+                                <FontAwesomeIcon icon = {showQuarterFinals ? faChevronUp : faChevronDown} />
+                            </span>
+                        </button>
+                        {showQuarterFinals && quarterFinalsWinners.length > 0 && quarterFinalMatches.length > 0 && (
+                            <div className = "flex-1 shadow-xl rounded-[12px]">
+                                <WinnersTable winners = {quarterFinalsWinners} matches = {quarterFinalMatches} />
+                            </div>
+                    )}
+                    </div>
+                    <div className = "flex flex-col">
+                        <button className = "text-xl font-bold shadow-lg p-2 rounded-[12px]" onClick = {toggleSemiFinals}>
+                            View Semi Finals
+                            <span className = "text-xl ml-2">
+                                <FontAwesomeIcon icon = {showSemiFinals ? faChevronUp : faChevronDown} />
+                            </span>
+                        </button>
+                        {showSemiFinals && semiFinalsWinners.length > 0 && semiFinalMatches.length > 0 && (
+                            <div className="flex-1 shadow-xl rounded-[12px]">
+                                <WinnersTable winners = {semiFinalsWinners} matches = {semiFinalMatches} />
                             </div>
                         )}
-                        renderSeedComponent = {CustomSeed}
-                    />
+                    </div>
+                    <div className = "flex flex-col">
+                        <button className = "text-xl font-bold shadow-lg p-2 rounded-[12px]" onClick = {toggleFinals}>
+                            View Finals
+                            <span className="text-lg ml-2">
+                                <FontAwesomeIcon icon = {showFinals ? faChevronUp : faChevronDown} />
+                            </span>
+                        </button>
+                        {showFinals && (
+                            <>
+                                {finalsMatch && finalsMatch.length > 0 ? (
+                                    <div className = "flex-1">
+                                        <WinnersTable winners = {tournamentWinner} matches = {finalsMatch} />
+                                    </div>
+                                ) : (
+                                    <div className = "flex-1 p-4 text-center text-lg font-semibold text-gray-500">
+                                        No Finals matches available.
+                                    </div>
+                                )}
+                            </>
+                        )}
+                    </div>
                 </div>
-            )}
-        </div>
+            </div>
+        </>
     );
 };
 
